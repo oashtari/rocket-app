95edd7b35408cf15e766874411fccbab6453cf1c
omidashtari <oashtari@gmail.com>
2023-06-17 06:22:46 -0700

    added routes and basic auth, forgot to push after just routes

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,65 @@
 #[macro_use] // ensures we can use the macros in rocket
 extern crate rocket;
 
+use rocket::http::hyper::request;
+use rocket::http::Status;
+use rocket::request::{FromRequest, Outcome, Request};
 use rocket::response::status;
 use rocket::serde::json::{json, Value}; // this json macros converts data to json file
 
+pub struct BasicAuth {
+    pub username: String,
+    pub password: String,
+}
+
+impl BasicAuth {
+    fn from_authorization_header(header: &str) -> Option<BasicAuth> {
+        let split = header.split_whitespace().collect::<Vec<_>>();
+
+        if split.len() != 2 {
+            return None;
+        }
+
+        if split[0] != "Basic" {
+            return None;
+        }
+
+        Self::from_base64_encoded(split[1])
+    }
+
+    fn from_base64_encoded(base64_string: &str) -> Option<BasicAuth> {
+        let decoded = base64::decode(base64_string).ok()?;
+        let decoded_str = String::from_utf8(decoded).ok()?;
+        let split = decoded_str.split(":").collect::<Vec<_>>();
+
+        if split.len() != 2 {
+            return None;
+        }
+
+        let (username, password) = (split[0].to_string(), split[1].to_string());
+
+        Some(BasicAuth { username, password })
+    }
+}
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for BasicAuth {
+    type Error = ();
+
+    async fn from_request(request: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        let auth_header = request.headers().get_one("Authorization");
+        if let Some(auth_header) = auth_header {
+            if let Some(auth) = Self::from_authorization_header(auth_header) {
+                return Outcome::Success(auth);
+            }
+        }
+
+        Outcome::Failure((Status::Unauthorized, ()))
+    }
+}
+
 #[get("/rustaceans")]
-fn get_rustaceans() -> Value {
+fn get_rustaceans(_auth: BasicAuth) -> Value {
     json!([{"id": 1, "name":"John Doe"}, {"id" : 2, "name":"John Doe again"}])
 }
 
@@ -29,6 +83,11 @@ fn delete_rustacean(_id: i32) -> status::NoContent {
     status::NoContent
 }
 
+#[catch(404)]
+fn not_found() -> Value {
+    json!("Not found!")
+}
+
 #[rocket::main]
 async fn main() {
     let _ = rocket::build()
@@ -42,6 +101,7 @@ async fn main() {
                 delete_rustacean
             ],
         )
+        .register("/", catchers![not_found])
         .launch()
         .await;
 }
