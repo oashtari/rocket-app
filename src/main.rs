d55e88d907ba1b4fd9c800525a22a90b5e71b0c6
omidashtari <oashtari@gmail.com>
2023-06-19 08:55:58 -0700

    wrote POST query, created new struct for new rustaceans

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,9 @@ mod schema;
 
 use auth::BasicAuth;
 use diesel::prelude::*;
-use models::Rustacean;
+use models::{NewRustacean, Rustacean};
 use rocket::response::status;
-use rocket::serde::json::{json, Value}; // this json macros converts data to json file
+use rocket::serde::json::{json, Json, Value}; // this json macros converts data to json file
 use rocket_sync_db_pools::database;
 use schema::rustaceans;
 
@@ -35,9 +35,20 @@ fn view_rustacean(id: i32, _auth: BasicAuth) -> Value {
     json!({"id": id, "name":"John Doe", "email":"john@doe.com"})
 }
 
-#[post("/rustaceans", format = "json")]
-fn create_rustacean(_auth: BasicAuth) -> Value {
-    json!({"id": 3, "name":"John Doe", "email":"john@doe.com"})
+#[post("/rustaceans", format = "json", data = "<new_rustacean>")]
+async fn create_rustacean(
+    _auth: BasicAuth,
+    db: DbConn,
+    new_rustacean: Json<NewRustacean>,
+) -> Value {
+    db.run(|c| {
+        let result = diesel::insert_into(rustaceans::table)
+            .values(new_rustacean.into_inner())
+            .execute(c)
+            .expect("DB error when inserting ");
+        json!(result)
+    })
+    .await
 }
 
 #[put("/rustaceans/<id>", format = "json")]
